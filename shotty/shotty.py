a867f68987f1d906b532ddb05403689f05f4d95b
Luis Cardona <lcardonag@gmail.com>
2020-05-14 09:55:11 -0500

    Prevent trying to create snapshots on intaces with an snapshot in progress

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -14,17 +14,29 @@ def filter_instances(project):
         instances=ec2.instances.all()
     return instances
 
+
+def has_pending_snapshot(volume):
+    snapshots=list(volume.snapshots.all())
+
+    return snapshots and snapshots[0].state=='pending'
+
+
+
 @click.group()
 def cli():
-    """Shotty manages snapshots and instances"""
+    """Shotty manages EC2 AWS snapshots and instances"""
 
 @cli.group('snapshots')
 def snapshots():
     """Commands for snapshots"""
 @snapshots.command('list')
-@click.option('--project',default=None, help="Only snapshots for project (tag Project:<name>)")
+@click.option('--project',default=None, 
+    help="Only snapshots for project (tag Project:<name>)")
 
-def list_snapshots(project):
+@click.option('--all','list_all',default=False, is_flag=True, 
+    help="List all snapshots for each volume, not just the most recent one")
+
+def list_snapshots(project,list_all):
     """List of EC2 Snapshots"""
 
     instances= filter_instances(project)
@@ -39,6 +51,8 @@ def list_snapshots(project):
                 s.progress,
                 s.start_time.strftime("%c")
                 )))
+
+                if s.state == 'completed' and not list_all: break
     return
 
 
@@ -78,6 +92,9 @@ def create_snapshots(project):
         i.stop()
         i.wait_until_stopped()
         for v in i.volumes.all():
+            if has_pending_snapshot(v):
+                print("Skipping {0}, snapshot already in progress.".format(v.id))
+                continue
             print("Creating snapshot for {0}".format(v.id))
             v.create_snapshot(Description="Created by Snappy 3000")
         print("Starting {0} ...".format(i.id))
